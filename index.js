257245839347c1647fc4ed73647bae97a8ad72ff
Pardeep Singh <PardeepSinghBatth@gmail.com>
2025-12-19 16:11:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/test',(req,res)=>{
+    res.send('test route')
+})
+
+app.get('/test-2',(req,res)=>{
+    res.send('test-2 route')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
